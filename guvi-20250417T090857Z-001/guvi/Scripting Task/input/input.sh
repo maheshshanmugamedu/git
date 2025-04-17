c715fc326303e6f9d4f09c052c1a9f00eb043fcb
maheshshanmugamedu <maheshshanmugamedu@gmail.com>
2025-04-17 15:14:43 +0530

    updates in git

--- guvi-20250417T090857Z-001/guvi/Scripting Task/input/input.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# File to modify
-FILE="input.txt"
-
-# Using sed to replace 'give' with 'learning' from the 5th line onward in lines containing 'welcome'
-sed -i '5,$ {/welcome/s/give/learning/g}' "$FILE"
-
--- guvi-20250417T090857Z-001/guvi/Scripting Task/input/input2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# File to modify
-FILE="input.txt"
-
-# Using awk to replace 'give' with 'learning' from the 5th line onward in lines containing 'welcome'
-awk 'NR < 5 { print; next } /welcome/ { gsub(/give/, "learning") } { print }' "$FILE" > temp.txt && mv temp.txt "$FILE"
-
